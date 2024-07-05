a72a417e8794ed77d73d9f2b5f1860f9a7f7c050
Custy <69308633+Custyy@users.noreply.github.com>
2024-07-05 03:27:24 +0300

    ,

--- models/Test.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const testSchema = new mongoose.Schema({
-    guildID: String,
-    userID: String,
-    data: Object
-})
-
-module.exports = mongoose.model('Test', testSchema)
--- util/eventLoader.js
+++
@@ -1,5 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`)
-
-module.exports = client => {
-  client.on('messageCreate', reqEvent('messageCreate'))
-}
